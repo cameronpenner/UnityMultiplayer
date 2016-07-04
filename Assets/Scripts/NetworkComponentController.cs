69f5ae11caad999c1e1234863ff8cfec9597ca54
CameronPenner <camdpenner@gmail.com>
2016-07-04 14:39:34 -0500

    Comments and cleanup

--- Assets/Scripts/NetworkComponentController.cs
+++ Assets/Scripts/NetworkComponentController.cs
@@ -5,15 +5,19 @@ using UnityEngine.Networking;
 public class NetworkComponentController : NetworkBehaviour
 {
 	[SerializeField]
+	[Tooltip("Components that should only be active for the local client")]
 	private Behaviour[] _clientBehaviours;
 
 	[SerializeField]
+	[Tooltip("Gameobjects that should only be active for the local client")]
 	private GameObject[] _clientGameObjects;
 
 	private void Start()
 	{
+		//activate the NetworkAnimator so it syncs for all players
 		GetComponent<NetworkAnimator>().SetParameterAutoSend(0, true);
 
+		//disable components that should only be active for the local client
 		if(!isLocalPlayer)
 		{
 			foreach(var behaviour in _clientBehaviours)
