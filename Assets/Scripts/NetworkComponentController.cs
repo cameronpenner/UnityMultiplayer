1160d58cacc2c6176c5383e76cd0e80e64147ff7
CameronPenner <camdpenner@gmail.com>
2016-07-04 10:59:03 -0500

    Local controls implemented

---
+++ Assets/Scripts/NetworkComponentController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class NetworkComponentController : NetworkBehaviour
+{
+	[SerializeField]
+	private Behaviour[] _clientBehaviours;
+
+	[SerializeField]
+	private GameObject[] _clientGameObjects;
+
+	private void Start()
+	{
+		if(!isLocalPlayer)
+		{
+			foreach(var behaviour in _clientBehaviours)
+			{
+				behaviour.enabled = false;
+			}
+
+			foreach(var gameObject in _clientGameObjects)
+			{
+				gameObject.SetActive(false);
+			}
+		}
+	}
+}
